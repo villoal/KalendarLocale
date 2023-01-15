02e5a638b5bf24150bfa2d8378471f414a44e4bd
Vicente <villoal@alumni.uv.es>
2023-01-15 18:15:28 +0100

    6th attempt jitpack

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     `kotlin-dsl`
     kotlin("jvm") version "1.6.10"
@@ -13,14 +14,17 @@ dependencies {
     testImplementation("junit", "junit", "4.12")
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "com.github.villoal"
-            artifactId = "KalendarLocale"
-            version = "0.1.1"
+project.afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "com.github.villoal"
+                artifactId = "KalendarLocale"
+                version = "0.1.3"
 
-            from(components["java"])
+                from(components["java"])
+            }
         }
     }
+
 }
